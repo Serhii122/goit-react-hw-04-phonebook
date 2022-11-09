83a907283bd806da2ec5b1d95cd7063e853ac81b
Serhii Hudzenko <12gserij12@gmail.com>
2022-11-09 18:12:20 +0200

    copy

---
+++ src/components/App.styled.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Form = styled.div`
+  margin-left: 30px;
+  margin-top: 30px;
+  margin-bottom: 30px;
+
+  h1 {
+    font-size: 24px;
+  }
+
+  h2 {
+    font-size: 16px;
+  }
+`;
---
+++ src/components/ContactList/ContactList.styled.jsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const StyledContacts = styled.ul`
+  font-size: 16px;
+  margin-top: 25px;
+  padding: 10px;
+`;
+
+export const ContactsItem = styled.li`
+  display: flex;
+  width: 300px;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+ 
+
+  &:not(:last-child) {
+    margin-bottom: 8px;
+  }
+
+  button {
+    padding: 2px 7px;
+    margin-left: 20px;
+    background-color: white;
+    border: 1px solid grey;
+    border-radius: 4px;
+    font-size: 16px;
+    cursor: pointer;
+  }
+`;
